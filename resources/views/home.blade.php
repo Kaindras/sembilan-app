393c24900315d2d84157ddd439064f4004a62b62
Kaindras <kaindras2005@gmail.com>
2025-09-02 20:53:23 +0800

    Finalisasi Blade Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,118 +1,31 @@
  <x-layout :title="$title">
    
-     {{-- <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-       <!-- Your content -->
-       <div class="w-100 h-100 bg-amber-400 text-white p-0.5 me-0.5 text-shadow-xs grid place-items-center">BPPMHKP DENPASAR
-         <img src="/img/9.png" alt="logo sembilan">
-       </div>
-  
-     </div> --}}
-  <section class="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
+ 
+  <section class="py-4 bg-white md:py-16 dark:bg-gray-900 antialiased">
     <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0">
       <div class="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
         <div class="shrink-0 max-w-md lg:max-w-lg mx-auto">
-          <img class="w-full dark:hidden" src="/img/logo_app.png" alt="logo_app" alt="" />
-          <img class="w-full hidden dark:block" src="/img/logo_app.png" alt="logo_app" alt="" />
+          <img class="w-1/2 md:w-1/3 lg:w-1/4 mx-auto dark:hidden" src="/img/logo_app.png" alt="logo_app" alt="Logo Sembilan" />
+          <img class="w-1/2 md:w-1/3 lg:w-1/4 mx-auto hidden dark:block" src="/img/logo_app.png" alt="logo_app" alt="Logo Sembilan" />
         </div>
-
         <div class="mt-6 sm:mt-8 lg:mt-0">
           <h1
-            class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white"
-          >
+            class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
             BADAN PENGENDALIAN DAN PENGAWASAN MUTU HASIL KELAUTAN DAN PERIKANAN (BPPMHKP)
           </h1>
           <div class="mt-4 sm:items-center sm:gap-4 sm:flex">
-            <p
-              class="text-2xl font-extrabold text-gray-500 sm:text-3xl dark:text-white"
-            >
+            <p class="text-2xl font-extrabold text-gray-500 sm:text-3xl dark:text-white">
             DENPASAR
             </p>
 
             <div class="flex items-center gap-2 mt-2 sm:mt-0">
               <div class="flex items-center gap-1">
-                {{-- <svg
-                  class="w-4 h-4 text-yellow-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"
-                  />
-                </svg>
-                <svg
-                  class="w-4 h-4 text-yellow-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"
-                  />
-                </svg>
-                <svg
-                  class="w-4 h-4 text-yellow-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"
-                  />
-                </svg> --}}
                 
               </div>
-              {{-- <p
-                class="text-sm font-medium leading-none text-gray-500 dark:text-gray-400"
-              >
-                (5.0)
-              </p>
-              <a
-                href="#"
-                class="text-sm font-medium leading-none text-gray-900 underline hover:no-underline dark:text-white"
-              >
-                345 Reviews
-              </a> --}}
             </div>
           </div>
 
           <div class="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
-            {{-- <a
-              href="#"
-              title=""
-              class="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-              role="button"
-            >
-              <svg
-                class="w-5 h-5 -ms-2 me-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
-                />
-              </svg>
-              Add to favorites
-            </a> --}}
-
-            {{--  --}}
             
           </div>
 
@@ -121,11 +34,45 @@
          
 
           <p class="text-gray-500 dark:text-gray-400">
-            JALAN SUNSET ROAD NOMOR 777 KUTA BADUNG BALI 80361</p>
-<p class="text-gray-500 dark:text-gray-400">TELEPON (0361) 4727390, FAKSIMILE (0361) 8477427</p>
-<p class="text-gray-500 dark:text-gray-400">LAMAN www.kkp.go.id, SURAT ELEKTRONIK ses.1013000000@kkp.go.id
-            
-          </p>
+            Jalan Sunset Road Nomor 777 Kuta Badung Bali 80361</p>
+<p class="text-gray-500 dark:text-gray-400">Telepon (0361) 4727390, Faksimile (0361) 8477427</p>
+<p class="text-gray-500 dark:text-gray-400"><a href="https://kkp.go.id" target="_blank" class="text-gray-500 dark:text-gray-400" >Laman www.kkp.go.id,</a> Surat Elektronik ses.1013000000@kkp.go.id </p>
+
+
+<div>
+  
+<ul class="flex space-x-4"> <p class="flex text-gray-500 dark:text-gray-400"> Sosial Media : 
+  <li>
+  <a href="https://www.instagram.com/username" target="_blank">
+  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
+</svg>
+</a>
+</li>
+
+<li>
+{{-- facebook --}}
+
+   <a href="https://www.facebook.com/username" target="_blank">
+   <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd"/>
+</svg>
+</a>
+</li>
+{{-- twitter --}}
+
+  <a href="https://x.com/username" target="_blank" class="text-black hover:text-gray-700">
+  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M22 5.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.343 8.343 0 0 1-2.605.981A4.13 4.13 0 0 0 15.85 4a4.068 4.068 0 0 0-4.1 4.038c0 .31.035.618.105.919A11.705 11.705 0 0 1 3.4 4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 6.1 13.635a4.192 4.192 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 2 18.184 11.732 11.732 0 0 0 8.291 20 11.502 11.502 0 0 0 19.964 8.5c0-.177 0-.349-.012-.523A8.143 8.143 0 0 0 22 5.892Z" clip-rule="evenodd"/>
+</svg></a></li>
+</ul></p>
+
+
+<hr class="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
+
+<div class="flex">
+  
+<img class="w-20 h-10" src="/img/kaindras.png"><p class="text-gray-500 dark:text-gray-400 font-sx">Supported by the Management of the Denpasar KKP Quality Agency </p> </div>
         </div>
       </div>
     </div>
@@ -134,3 +81,12 @@
     </x-layout>
   
 
+ {{-- <svg xmlns="http://www.w3.org/2000/svg" width="300" height="100" viewBox="0 0 300 100">
+  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"
+        font-family="'Brush Script MT', 'Segoe Script', cursive" 
+        font-size="20" 
+        font-weight="100"
+        fill="black">
+    Kaindras
+  </text> --}}
+{{-- </svg> --}}
