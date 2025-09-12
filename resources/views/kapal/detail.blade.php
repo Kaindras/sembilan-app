dfe0fae3e9af6f9b1444212a0aa43c482d9734cf
Kaindras <kaindras2005@gmail.com>
2025-09-12 14:41:18 +0800

    modified table sbk spps

--- resources/views/kapal/detail.blade.php
+++ resources/views/kapal/detail.blade.php
@@ -9,12 +9,18 @@
                   <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                       <div>
                         <div>
-        <img class="h-auto max-w-xs rounded-lg" src="{{ asset('storage/' . $kapal->foto) }}" alt="{{ $kapal->nama_kapal}}" id="foto-preview">
-    </div>
-                          <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $kapal->nama_kapal }}</a>
-                          <p class="text-base text-gray-500 dark:text-gray-400">{{ $kapal->pemilik->nama }}</p>
+                            <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $kapal->nama_kapal }}</a>
+                        <img class="h-auto max-w-xs rounded-lg" src="{{ asset('storage/' . $kapal->foto) }}" alt="{{ $kapal->nama_kapal}}" id="foto-preview">
+                        </div>
+                          
+                          <p class="text-base text-gray-900 dark:text-gray-400">Pemilik : {{ $kapal->pemilik->nama }}</p>
                           <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022">Alamat : {{ $kapal->pemilik->alamat }}</time></p>
                           <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022">Tanggal Inspeksi : {{ $kapal->tgl_inspeksi }}</time></p>
+                          <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022">Petugas Inspeksi :1. {{ $kapal->inspektur->name }}</time></p>
+                          <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022">Petugas Inspeksi :2. {{ $kapal->inspektur->name }}</time></p>
+                          <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022">Petugas Inspeksi :3. {{ $kapal->inspektur->name }}</time></p>
+                          <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate datetime="2022-02-08" title="February 8th, 2022">Petugas Inspeksi :4. {{ $kapal->inspektur->name }}</time></p>
+                          
                       </div>
                   </div>
               </address>
@@ -30,51 +36,64 @@
                     <td><strong>Nama Kapal</strong></td>
                       <td>{{ $kapal->nama_kapal }}</td>
                     </tr>
+                    
                     <tr>
-
                     <td><strong>Sertifikat</strong></td>
                     <td>{{ $kapal->no_sertifikat }}</td>
                     </tr>
+
+                <tr>
+                    <td><strong>Masa Berlaku</strong></td>
+                    <td>{{ $kapal->no_sertifikat }}</td>
+                </tr>
 
                     <tr>
                     <td><strong>Grade</strong></td>
                     <td>{{ $kapal->grade }}</td>
                     </tr>
+                    <tr>
+                    <td><strong>Awak Kapal Bersertifikat CPIB Awak Kapal</strong></td>
+                    <td>{{ $kapal->grade }}</td>
+                    </tr>
 
                     <tr>
-                     <td><strong>Ukuran</strong></td>
-                    <td>{{ $kapal->ukuran}} M</td>
+                    <td><strong>Memilik Personil Tetap Penanggung Jawab Mutu (QA)</strong></td>
+                    <td>{{ $kapal->grade }}</td>
+                    </tr>
+
+                    <tr>
+                    <td><strong>Mempunyai Manual HACPP</strong></td>
+                    <td>{{ $kapal->grade }}</td>
+                    </tr>
+
+                    <tr>
+                     <td><strong>Ukuran Kapal</strong></td>
+                    <td>{{ $kapal->ukuran}} GT </td>
                     </tr>
                   
                      <tr>
-                     <td><strong>Daerah Penangakapan</strong></td>
-                    <td>{{ $kapal->daerah_tangkap}}</td>
+                     <td><strong>Daerah Penangkapan</strong></td>
+                    <td>WPPRI/ FAO {{ $kapal->daerah_tangkap}}</td>
                     </tr>
 
-                     <tr>
-                     <td><strong>Alat Tangkap</strong></td>
-                    <td>{{ $kapal->alat_tangkap}}</td>
+                <td><strong>Lama Trip</strong></td>
+                    <td>{{ $kapal->lama_trip}}  Bulan</td>
                     </tr>
-
-                    <tr>
-                     <td><strong>Lama Trip</strong></td>
-                    <td>{{ $kapal->lama_trip}}</td>
-                    </tr>
-                     <tr>
-                     <td><strong>Jenis Ikan</strong></td>
-                    <td>{{ $kapal->jenis_ikan}}</td>
-                    </tr>
-                     <tr>
-                     <td><strong>Penanganan</strong></td>
-                    <td>{{ $kapal->penanganan}}</td>
-                    </tr>
-                     <tr>
-                     <td><strong>Suhu</strong></td>
-                    <td>{{ $kapal->suhu}}</td>
-                    </tr>
+                <tr>
+                   <td><strong>Alat Tangkap</strong></td>
+                   <td>{{ $kapal->alat_tangkap}}</td>
+                </tr>
+                <tr>
+                     <td><strong> Hasil Tangkapan</strong></td>
+                    <td>Fresh / Frozen {{ $kapal->jenis_ikan}}</td>
+                </tr>
+                <tr>
+                     <td><strong>Suhu Produk</strong></td>
+                    <td> -{{ $kapal->suhu}} Celcius</td>
+                </tr>
                      <tr>
                      <td><strong>Suhu Palka</strong></td>
-                    <td>{{ $kapal->suhu_palka}}</td>
+                    <td>-{{ $kapal->suhu_palka}} Celcius</td>
                     </tr>
                      <tr>
                      <td><strong>Nilai Organoleptik</strong></td>
@@ -85,30 +104,40 @@
                     <td>{{ $kapal->estimasi_berat}}</td>
                     </tr>
                      <tr>
-                     <td><strong>Tipe Kapal</strong></td>
+                     <td><strong>Jenis Kapal</strong></td>
                     <td>{{ $kapal->tipe_kapal}}</td>
                     </tr>
                      <tr>
                      <td><strong>Tanggal_inspeksi</strong></td>
                     <td>{{ $kapal->tanggal_inspeksi}}</td>
                     </tr>
-                     <tr>
-                     <td><strong>Pelabuhan Pangakalan</strong></td>
+                    
+                <tr>
+                    <td><strong>Pelabuhan Domisili (Asal)</strong></td>
                     <td>{{ $kapal->pelabuhan_pangkalan}}</td>
-                    </tr>
+                </tr>
+                <tr>
+                    <td><strong>Izin Pelabuhan Pangakalan (Sandar) 1. </strong></td>
+                    <td>{{ $kapal->pelabuhan_pangkalan}}</td>
+                </tr>
+
+                   <tr>
+                    <td><strong>Izin Pelabuhan Pangakalan (Sandar) 2. </strong></td>
+                    <td>{{ $kapal->pelabuhan_pangkalan}}</td>
+                </tr>
                      <tr>
                      <td><strong>Status Akfif</strong></td>
                     <td>{{ $kapal->kapal_aktif}}</td>
                     </tr>
-                     <tr>
+
+                     {{-- <tr>
                      <td><strong>Uraian</strong></td>
                     <td>{{ $kapal->uraian}}</td>
                     </tr>
-
                     <tr>
                      <td><strong>Catatan Inspeksi</strong></td>
                     <td>{{ $kapal->catatan}}</td>
-                    </tr>
+                    </tr> --}}
                      <tr>
                      <td><strong>Petugas Inspeksi</strong></td>
                     <td>{{ $kapal->inspektur->name}}</td>
