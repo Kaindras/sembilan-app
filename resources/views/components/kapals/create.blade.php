199e4fd6e5a656043090653771aafb0019196394
Kaindras <kaindras2005@gmail.com>
2025-09-12 15:54:39 +0800

    hapus dd

--- resources/views/components/kapals/create.blade.php
+++ resources/views/components/kapals/create.blade.php
@@ -40,7 +40,7 @@
                     <p class="mt-2 text-xs text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
                     @enderror
             </div>
-            @dd('/create')
+        
 
             <div class="mb-4">
                 <label for="pemilik" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Pemilik</label><select name="pemilik_id" id="pemilik" class="@error('pemilik_id') bg-red-50 bg-gray-50 border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 @enderror border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
